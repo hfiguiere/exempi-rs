575fd3792fa04a43a936860dd3f8673e1b4b93f5
Hubert Figuière <hub@figuiere.net>
2016-01-07 00:56:54 -0500

    Initial attempt to bind Exempi

---
+++ build.rs
@@ -0,0 +1,12 @@
+extern crate pkg_config;
+
+fn main() {
+    match pkg_config::find_library("exempi-2.0") {
+        Ok(_) => 
+		(),
+        Err(e) => {
+            println!("Exempi not found");
+            panic!(e);
+        }
+    }
+}
