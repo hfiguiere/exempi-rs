91e579e167bf55fcbf1921b251bd145c93a47043
Hubert Figuière <hub@figuiere.net>
2017-01-07 15:57:31 -0500

    exempi-sys: require exempi 2.4.0

--- exempi-sys/build.rs
+++ exempi-sys/build.rs
@@ -1,9 +1,9 @@
 extern crate pkg_config;
 
 fn main() {
-    match pkg_config::find_library("exempi-2.0 >= 2.2.0") {
+    match pkg_config::find_library("exempi-2.0 >= 2.4.0") {
         Ok(_) =>
-		(),
+	    (),
         Err(e) => {
             println!("Exempi not found");
             panic!(e);
